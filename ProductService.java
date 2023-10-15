194c9bb1a943bc031b54bcac58aa4f5a3f070b02
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2023-10-15 13:27:37 +0530

    Adding Service Layer, Product Service

---
+++ ProductService.java
@@ -0,0 +1,31 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Optional<Product> findById(String id) {
+        return productRepository.findById(id);
+    }
+
+    public List<Product> findAll() {
+        return (List<Product>) productRepository.findAll();
+    }
+
+    public void deleteById(String id) {
+        productRepository.deleteById(id);
+    }
+}
